e4f4b53c490f2a5be191ba8875a29acb55410ec8
shahruhisaev <isaevshahruh2001@gmail.com>
2026-02-02 21:52:47 +0500

    created services;

---
+++ services/firstService/src/main/java/kz/app/firstservice/FirstServiceApplication.java
@@ -0,0 +1,12 @@
+package kz.app.firstservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstServiceApplication.class, args);
+    }
+}
